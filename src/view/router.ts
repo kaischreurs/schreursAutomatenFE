312f86f74baaeedd6b0e04021c14908186ed4298
kaischreurs <70720881+kaischreurs@users.noreply.github.com>
2024-07-19 23:14:30 +0200

    wat?

--- src/view/router.ts
+++ src/view/router.ts
@@ -4,9 +4,9 @@ import "./pages/notfound-page";
 import "./pages/machine-page";
 
 router.setRoutes([
-  { path: `${BASE}/`, component: "machine-page" },
   {
     path: `${BASE}/machine`,
     component: "machine-page",
   },
+  { path: "(.*)", component: "notfound-page" },
 ]);
